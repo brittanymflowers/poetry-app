3acbeac4dfdfae6ff2c85301907358a3caee71a8
brittanymflowers <brittanymflowers@gmail.com>
2024-10-27 23:15:16 -0600

    app changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TableComponent } from './components/table/table.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchBarComponent, TableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'poetry-app';
+  searchTypeAuthor = 'Author';
+  searchTypeTitle = 'Title';
 }
